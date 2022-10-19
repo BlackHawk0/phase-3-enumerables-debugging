5c2e7d9acc4bfe07f020fef0fe65314bcaf96fd0
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-19 13:30:36 +0300

    reverse

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,12 @@
-# Write your code here
-require 'pry'
+# require 'pry'
 def reverse_each_word str
     words = str.split
-    reversed_word = []
+    # reversed_word = words.map do |word|
+    #     reversed_word << word.reverse
+    # end
 
-    # binding.pry
-    words.map do |word|
-        reversia = ""
-        i = 0
-        while i < word.length
-            reversia = word[i] + reversia
-            i += 1
-            reversia
-        end
-        reversed_word = reversed_word.push(reversia)
-    end
-    puts reversed_word
-    reversed_word
+    reversed_word = words.map(&:reverse).join(" ")
+ 
 end
-reverse_each_word("Hello there and how are you?")
+
+pp reverse_each_word("Hello there and how are you?")
